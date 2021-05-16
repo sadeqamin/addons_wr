5f8700046f2e9560e75c28ffb61474fad4713dab
user <user@debian.localhost>
2021-05-16 09:40:18 +0300

    sales dsicount-160501

--- sales_discount_workflow/sale.py
+++ sales_discount_workflow/sale.py
@@ -4,18 +4,3 @@
 
 from odoo import api, fields, models, tools, _
 
-class PosOrder(models.Model):
-    _inherit = "pos.order"
-
-
-
-    def _force_picking_done(self, picking):
-        """Force picking in order to be set as done."""
-        self.ensure_one()
-        res = super(PosOrder, self)._force_picking_done(picking)
-
-        if picking.state == 'assigned':
-        	picking.action_pack_operation_auto_fill()
-            picking.do_new_transfer()
-
-        return res
